34bf252ca759face68f6834cbfc57f1113f6b85b
TROCADER <60465392+TROCADER@users.noreply.github.com>
2020-03-11 11:07:54 +0100

    Lektion 2020-03-11

--- Slutprojektet PRR1 2020/Slutprojektet PRR1 2020/Program.cs
+++ Slutprojektet PRR1 2020/Slutprojektet PRR1 2020/Program.cs
@@ -36,7 +36,7 @@ namespace Slutprojektet_PRR1_2020
 
             int.TryParse(Console.ReadLine().Trim(), out int playerInput);
 
-            //Fixa smrt kod genom DRY här:
+            //Fixa smart kod genom DRY här:
             while (playerInput != 1 && playerInput != 2)
             {
                 Console.WriteLine("Du har valt ett spel som inte existerar, vänligen välj igen");
@@ -45,11 +45,71 @@ namespace Slutprojektet_PRR1_2020
 
             if (playerInput == 1)
             {
+                Console.Clear();
                 Console.WriteLine("Du har valt Blackjack");
                 Console.WriteLine("Du kommer nu bli skickad till det valda spelet");
 
-                Thread.Sleep(1000);
+                Thread.Sleep(2000);
+                Console.Clear();
+
+                BlackJack();
             }
+        }
+
+        static void BlackJack()
+        {
+            LoadGame();
+
+            string[] options =
+            {
+                "1: Börja spela",
+                "2: Lär dig spela"
+            };
+
+            Console.WriteLine("Hej och välkommen till spelet Blackjack!");
+            Console.WriteLine("\nVad vill du göra?");
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine(options[i]);
+            }
+
+            PlayerChoose();
+
+            if ()
+            {
+
+            }
+
+            Console.WriteLine();
+        }
+
+        static void LoadGame()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("Loading game, please wait...");
+
+            string[] dots = new string[5];
+
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i] = ".";
+            }
+
+            for (int i = 0; i < dots.Length; i++)
+            {
+                Console.WriteLine(dots[i]);
+                Thread.Sleep(500);
+            }
+
+            Console.Clear();
+        }
+
+        static int PlayerChoose()
+        {
+            int.TryParse(Console.ReadLine().Trim(), out int result);
+
+            return result;
         }
     }   
 }
